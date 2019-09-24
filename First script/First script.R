aade16c24b004362414b8bab7977b68055420671
DiegoDelPilar88 <dad143@georgetown.edu>
2019-09-23 20:37:16 -0400

    First class

--- First script/First script.R
+++ First script/First script.R
@@ -6,3 +6,23 @@ y<- 19
 z<- x-y
 
 save(x,y,z, file = "Output/ourdata.Rdata")
+
+# Lecture 1 example
+n<- rnorm(100)
+m<- 1+2*x + rnorm(100)
+plot(m,n)
+
+# Monte Carlo simulation 
+sample_size <- 100 #simulated sample size
+indep_var <- rnorm(sample_size) #independent variable
+error <- rnorm(sample_size) #simulated error
+# generate dependent variable as function of the independent variable and some error
+
+m = 1 + 2*indep_var + error
+
+#plot values
+plot (m,n)
+
+mtcars[1,]
+
+letters
---
+++ First script/Scratch.R
@@ -0,0 +1,3 @@
+# Here is my scratch work
+
+x2 <-5
